8158eda6b78e3195a82f104bf502f0da0f264f70
Prathamesh <Prathamesh@DESKTOP-MCEUBVJ>
2019-07-06 20:05:46 +0530

    change for non access modifiers

---
+++ JavaRecap/src/com/javatech/nonaccessmodifiers/NonStaticModifierTest.java
@@ -0,0 +1,16 @@
+package com.javatech.nonaccessmodifiers;
+
+public class NonStaticModifierTest {
+	
+	final double PI = 3.14; //cannot change the value of PI as it marked as Final
+	
+	public static void main(String[] args){
+		
+		
+		
+		
+	}
+	
+	
+
+}
