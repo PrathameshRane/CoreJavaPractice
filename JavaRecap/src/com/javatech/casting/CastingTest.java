b0fc351b1c11fb380306978b9c8f3b9b7ce9f818
AVN-TSUP-RAHUL <AVN-TSUP-RAHUL@AVN-TSUP-LAP-17>
2019-07-08 01:55:32 +0530

    casting

---
+++ JavaRecap/src/com/javatech/casting/CastingTest.java
@@ -0,0 +1,20 @@
+package com.javatech.casting;
+
+public class CastingTest {
+	
+	public static void main(String[] args){
+		
+		String s= "Casting";
+		
+		double d =45.5;
+	    char c ='c';
+		int a= (int)d;
+		System.out.println(s+a);
+		a=c;
+		System.out.println(a);
+		
+		
+		
+	}
+
+}
